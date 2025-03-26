f0de07b63bf7f9ed0ea09a79969124e6e2ed020d
varirus <varirusek@gmail.com>
2025-03-26 10:29:19 +0100

    sort, permutation, value

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -10,6 +10,7 @@
 using namespace std;
 
 struct Job {
+    int id;
     int r;
     int p;
     int q; 
@@ -29,6 +30,30 @@ vector<Job> sort_by_q(vector<Job> jobs) {
         return a.q > b.q;
     });
     return jobs;
+}
+
+// Sort by q value
+int objective_function_calc(vector<Job> jobs) {
+    int C_prev = 0;
+    int C_act = 0;
+    bool prime = true;
+    for (auto job : jobs)
+    {
+        if(prime)
+            C_prev = job.r + job.p;
+        else{
+            C_act = max(C_prev, job.r) + job.p;
+            C_prev = C_act;
+        }
+            
+        prime = false;
+    }
+    return C_act;
+}
+
+// Factorial function
+int fact(int n){
+    return (n==0) || (n==1) ? 1 : n* fact(n-1);
 }
 
 int main(int argc, char* argv[]) {
@@ -61,7 +86,7 @@ int main(int argc, char* argv[]) {
     stringstream ssa(line);
     int amount;
     ssa >> amount;
-
+    int id = 0;
     // Read record to vector
     for (int i = 0; i < amount; i++)
     {
@@ -69,24 +94,55 @@ int main(int argc, char* argv[]) {
         stringstream ss(line);
         int r,p,q;
         ss >> r >> p >> q;
-        jobs.push_back({r, p, q});
+        id++;
+        jobs.push_back({id, r, p, q});
     }
+   
+    vector<Job> best_permutation = jobs;
+    int best_value = 0;
+   
     printf("Nie posortowane\n");
     for (auto job : jobs)
     {
-        printf("%d %d %d\n", job.r, job.p, job.q);
+        printf("%d %d %d %d\n", job.id, job.r, job.p, job.q);
+        
     }
+    printf("C_act: %d\n", objective_function_calc(jobs));
     vector<Job> sort_r = sort_by_r(jobs);
     printf("Sortowane po r\n");
     for (auto job : sort_r)
     {
-        printf("%d %d %d\n", job.r, job.p, job.q);
+        printf("%d %d %d %d\n",job.id, job.r, job.p, job.q);
     }
+    printf("C_act: %d\n", objective_function_calc(sort_r));
     vector<Job> sort_q = sort_by_q(jobs);
     printf("Sortowane po q\n");
     for (auto job : sort_q)
     {
-        printf("%d %d %d\n", job.r, job.p, job.q);
+        printf("%d %d %d %d\n", job.id, job.r, job.p, job.q);
     }
-    
+    printf("C_act: %d\n", objective_function_calc(sort_q));
+    // Permutations
+    for (int i = 0; i < fact(amount); i++)
+    {
+        next_permutation(jobs.begin(), jobs.end(), [](const Job& a, const Job& b) {
+            return a.id > b.id;
+        });
+        //printf("Permutacja %d\n", i);
+        /*for (auto job : jobs)
+        {
+            printf("%d %d %d %d\n", job.id, job.r, job.p, job.q);
+        }*/
+        int value = objective_function_calc(jobs);
+        if(value > best_value){
+            best_value = value;
+            best_permutation = jobs;
+        }
+    }
+    printf("Najlepsze z permutacji\n");
+    for (auto job : best_permutation)
+    {
+        printf("%d %d %d %d\n", job.id, job.r, job.p, job.q);
+    }
+    printf("C_act: %d\n", best_value);
 }
