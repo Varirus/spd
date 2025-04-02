279678b9f68da8dd76364565e948de27e768cfac
varirus <varirusek@gmail.com>
2025-04-02 09:32:15 +0200

    upgraded structure of code

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -69,15 +69,31 @@ int compute_makespan_rpq(vector<Job> jobs) {
     return C_act;
 }
 
-// Factorial function
-int fact(int n){
-    return (n==0) || (n==1) ? 1 : n* fact(n-1);
+// Print Jobs
+void print_Jobs(vector<Job> jobs){
+    printf("id r p q\n");
+    for (auto job : jobs)
+    {
+        printf("%d. %d %d %d\n", job.id, job.r, job.p, job.q);
+        
+    }
+    printf("Makespan rp: %d\n", compute_makespan_rp(jobs));
+    printf("Makespan rpq: %d\n\n", compute_makespan_rpq(jobs));
 }
 
-int main(int argc, char* argv[]) {
+// Factorial function
+int fact(int n){
+    if(n == 1)
+        return 1;
+    else
+        return n*fact(n-1);
+}
+
+// Read input 
+vector<Job> readInput(int argc, char* argv[]){
     if (argc < 2) {
         cerr << "Correct usage : " << argv[0] << " [input file]" << endl;
-        return 1;
+        exit(1);
     }
 
     string filename = argv[1];
@@ -91,7 +107,7 @@ int main(int argc, char* argv[]) {
         file.open(filename);
         if (!file.is_open()) {
             cerr << "Error during opening file " << filename << endl;
-            return 1;
+            exit(1);
         }
         input = &file;
     }
@@ -105,6 +121,7 @@ int main(int argc, char* argv[]) {
     int amount;
     ssa >> amount;
     int id = 0;
+
     // Read record to vector
     for (int i = 0; i < amount; i++)
     {
@@ -115,72 +132,54 @@ int main(int argc, char* argv[]) {
         id++;
         jobs.push_back({id, r, p, q});
     }
-   
-    vector<Job> best_permutation_rp = jobs;
-    int best_value_rp = 0;
+    return jobs;
+}
 
-    vector<Job> best_permutation_rpq = jobs;
-    int best_value_rpq = 0;
-   
-    printf("Nie posortowane\n");
-    for (auto job : jobs)
-    {
-        printf("%d %d %d %d\n", job.id, job.r, job.p, job.q);
-        
-    }
-    printf("Makespan rp: %d\n", compute_makespan_rp(jobs));
-    printf("Makespan rpq: %d\n\n", compute_makespan_rpq(jobs));
-    vector<Job> sort_r = sort_by_r(jobs);
-    printf("Sortowane po r\n");
-    for (auto job : sort_r)
-    {
-        printf("%d %d %d %d\n",job.id, job.r, job.p, job.q);
-    }
-    printf("Makespan rp: %d\n", compute_makespan_rp(sort_r));
-    printf("Makespan rpq: %d\n\n",compute_makespan_rpq(sort_r));
-    vector<Job> sort_q = sort_by_q(jobs);
-    printf("Sortowane po q\n");
-    for (auto job : sort_q)
-    {
-        printf("%d %d %d %d\n", job.id, job.r, job.p, job.q);
-    }
-    printf("Makespan rp: %d\n", compute_makespan_rp(sort_q));
-    printf("Makespan rpq: %d\n\n", compute_makespan_rpq(sort_q));
-    // Permutations
-    printf("Factorial z %d to %d\n\n", amount, fact(amount));
-    for (int i = 0; i < fact(amount); i++)
+void bruteForce(vector<Job> jobs, vector<Job> &best_permutation_rp, vector<Job> &best_permutation_rpq){
+
+    int best_value_rp = 0, best_value_rpq = 0;
+
+    for (int i = 0; i < fact(jobs.size()); i++)
     {
         next_permutation(jobs.begin(), jobs.end(), [](const Job& a, const Job& b) {
             return a.id > b.id;
         });
-        //printf("Permutacja %d\n", i);
-        /*for (auto job : jobs)
-        {
-            printf("%d %d %d %d\n", job.id, job.r, job.p, job.q);
-        }*/
-        int value = compute_makespan_rp(jobs);
-        if(value > best_value_rp){
-            best_value_rp = value;
+        int value_rp = compute_makespan_rp(jobs);
+        if(value_rp > best_value_rp){
+            best_value_rp = value_rp;
             best_permutation_rp = jobs;
         }
-        value = compute_makespan_rpq(jobs);
-        if(value > best_value_rpq){
-            best_value_rpq = value;
+        int value_rpq = compute_makespan_rpq(jobs);
+        if(value_rpq > best_value_rpq){
+            best_value_rpq = value_rpq;
             best_permutation_rpq = jobs;
         }
     }
-    printf("Best Permutation rp\n");
-    for (auto job : best_permutation_rp)
-    {
-        printf("%d %d %d %d\n", job.id, job.r, job.p, job.q);
+}
+
+int main(int argc, char* argv[]) {
+    vector<Job> jobs = readInput(argc, argv);
+    vector<Job> best_permutation_rp;
+    vector<Job> best_permutation_rpq;
+    
+    printf("Nie posortowane\n");
+    print_Jobs(jobs);
+
+    printf("Sortowane po r\n");
+    vector<Job> sort_r = sort_by_r(jobs);
+    print_Jobs(sort_r);
+
+    printf("Sortowane po q\n");
+    vector<Job> sort_q = sort_by_q(jobs);
+    print_Jobs(sort_q);
+    
+    if(jobs.size() < 11){
+        bruteForce(jobs, best_permutation_rp, best_permutation_rpq);
+        printf("Best Permutation rp\n");
+        print_Jobs(best_permutation_rp);
+
+        printf("Best Permutation rpq\n");
+        print_Jobs(best_permutation_rpq);
     }
-    printf("Makespan rp: %d\n", best_value_rpq);
-    printf("Makespan rpq: %d\n\n", compute_makespan_rpq(best_permutation_rp));
-    printf("Best Permutation rpq\n");
-    for (auto job : best_permutation_rpq)
-    {
-        printf("%d %d %d %d\n", job.id, job.r, job.p, job.q);
-    }
-    printf("Makespan rp: %d\n", compute_makespan_rp(best_permutation_rpq));
-    printf("Makespan rpq: %d\n\n", best_value_rpq);
+    
 }
